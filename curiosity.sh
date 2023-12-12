9d15f860a740f71c4381594d52fc6684a347163b
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2023-12-12 15:38:46 -0500

    Update curiosity.sh

--- curiosity.sh
+++ curiosity.sh
@@ -2,7 +2,7 @@
 
 #!/data/data/com.termux/files/usr/bin/bash 
 # Interpretación determinada para la ejecución     
-COMANDOS = "pkg install git -y\npkg install nodejs -y\npkg install ffmpeg -y\npkg install imagemagick -y\npkg install -y yarn\ngit clone https://github.com/AzamiJs/CuriosityBot-MD\ncd CuriosityBot-MD\nyarn install\nnpm install\nnpm start"
+COMANDOS = "pkg install git -y\npkg install nodejs -y\npkg install ffmpeg -y\npkg install imagemagick -y\npkg install -y yarn\ngit clone https://github.com/Axel-bytu/Patobot\ncd Patobot\nyarn install\nnpm install\nnpm start"
 
 echo -e "\e[38;5;183m
 ─█▀▀█ ───░█ ░█─░█ ░█▀▀▀█ ▀▀█▀▀ ░█▀▀▀ 　 ░█─── ─█▀▀█ 　 ░█▀▀█ ─█▀▀█ ░█▄─░█ ▀▀█▀▀ ─█▀▀█ ░█─── ░█─── ─█▀▀█ 
